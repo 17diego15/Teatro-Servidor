b1c0bd90c2e4268e5e9c7c02be6d4a190faa476f
DESKTOP-481791G\S2-PC00 <licencias@svalero.com>
2024-02-20 18:47:23 +0100

    Context creado

---
+++ Data/Migrations/20240220174542_InitialCreate.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Actores",
+                columns: table => new
+                {
+                    ActorId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Actores", x => x.ActorId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Funciones",
+                columns: table => new
+                {
+                    FunciónID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    SalaID = table.Column<int>(type: "int", nullable: true),
+                    Fecha = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Hora = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Funciones", x => x.FunciónID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Obras",
+                columns: table => new
+                {
+                    ObraID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Titulo = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Director = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Sinopsis = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Duración = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Precio = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Imagen = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Obras", x => x.ObraID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Salas",
+                columns: table => new
+                {
+                    SalaID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Capacidad = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Salas", x => x.SalaID);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Actores",
+                columns: new[] { "ActorId", "Nombre" },
+                values: new object[] { 1, "Fernando" });
+
+            migrationBuilder.InsertData(
+                table: "Funciones",
+                columns: new[] { "FunciónID", "Fecha", "Hora", "SalaID" },
+                values: new object[] { 1, new DateTime(2024, 2, 20, 0, 0, 0, 0, DateTimeKind.Unspecified), "20:00", 1 });
+
+            migrationBuilder.InsertData(
+                table: "Obras",
+                columns: new[] { "ObraID", "Director", "Duración", "Imagen", "Precio", "Sinopsis", "Titulo" },
+                values: new object[] { 1, "gregrqe", "greqgre", "grgre", 12m, "gregq", "gfgrqe" });
+
+            migrationBuilder.InsertData(
+                table: "Salas",
+                columns: new[] { "SalaID", "Capacidad", "Nombre" },
+                values: new object[] { 1, 120, "gfgrqe" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Actores");
+
+            migrationBuilder.DropTable(
+                name: "Funciones");
+
+            migrationBuilder.DropTable(
+                name: "Obras");
+
+            migrationBuilder.DropTable(
+                name: "Salas");
+        }
+    }
+}
